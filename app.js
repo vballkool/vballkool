db84581fa53687d4d1a09687b173dfeed270024c
vballkool <newportgroup@protonmail.com>
2017-07-09 08:36:22 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var fs = require('fs');
  */
 var config = {
      address: '3PDxnbR1UTXrj84smhUP5m8WidRJjujfmCm',
-    startBlockHeight: 560975,
-    endBlock: 565975,
+    startBlockHeight: 565975,
+    endBlock: 572287,
     distributableMrtPerBlock: 30,
     filename: 'test4.json',
     node: 'http://5.189.156.200:6869',
